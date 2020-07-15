0aebaa8da54123ee83a84c1fddc4ad5703611d5b
Kiril Stas <vesground@gmail.com>
2020-07-15 11:58:57 +0300

    Build and push

---
+++ build/precache-manifest.c6865ab62024166c5bc5155d04b6ed28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6903a97f5eebc79fa98e647de02a8438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f01994e38d95624c85f",
+    "url": "/static/css/main.1a7da2c0.chunk.css"
+  },
+  {
+    "revision": "7bb17637a1a4ee93b2fe",
+    "url": "/static/js/2.c1f28b97.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c1f28b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f01994e38d95624c85f",
+    "url": "/static/js/main.46d7cb98.chunk.js"
+  },
+  {
+    "revision": "6046417e64ea64341b0b",
+    "url": "/static/js/runtime-main.a85dd691.js"
+  }
+]);
